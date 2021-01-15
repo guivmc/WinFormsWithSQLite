9964dea6b815a4c7f4941bd9722172cf0e95fe94
guivmc <guivmc@hotmail.com>
2021-01-14 22:41:34 -0300

    First

---
+++ ThomasGregTest/AcmeForm.Designer.cs
@@ -0,0 +1,304 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ThomasGregTest
+{
+    public partial class AcmeForm 
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;     
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BTNIncluir = new System.Windows.Forms.Button();
+            this.BTNExcluir = new System.Windows.Forms.Button();
+            this.LBLData = new System.Windows.Forms.Label();
+            this.LBLCusto = new System.Windows.Forms.Label();
+            this.LBLDistancia = new System.Windows.Forms.Label();
+            this.CHKCapturaSim = new System.Windows.Forms.CheckBox();
+            this.LBLNivelDor = new System.Windows.Forms.Label();
+            this.BTNSalvar = new System.Windows.Forms.Button();
+            this.BTNCancelar = new System.Windows.Forms.Button();
+            this.DBListView = new System.Windows.Forms.ListView();
+            this.CLNMID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CLNMData = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CLNMCaptura = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CLNMNivelDor = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CHKCapturaNao = new System.Windows.Forms.CheckBox();
+            this.LBLCaptura = new System.Windows.Forms.Label();
+            this.NumNivelDor = new System.Windows.Forms.NumericUpDown();
+            this.DTPData = new System.Windows.Forms.DateTimePicker();
+            this.NumCusto = new System.Windows.Forms.NumericUpDown();
+            this.NUMDistancia = new System.Windows.Forms.NumericUpDown();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.NumNivelDor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumCusto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUMDistancia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BTNIncluir
+            // 
+            this.BTNIncluir.Location = new System.Drawing.Point(600, 48);
+            this.BTNIncluir.Name = "BTNIncluir";
+            this.BTNIncluir.Size = new System.Drawing.Size(75, 23);
+            this.BTNIncluir.TabIndex = 0;
+            this.BTNIncluir.Text = "Incluir";
+            this.BTNIncluir.UseVisualStyleBackColor = true;
+            this.BTNIncluir.Click += new System.EventHandler(this.Incluir_Click);
+            // 
+            // BTNExcluir
+            // 
+            this.BTNExcluir.Location = new System.Drawing.Point(701, 48);
+            this.BTNExcluir.Name = "BTNExcluir";
+            this.BTNExcluir.Size = new System.Drawing.Size(75, 23);
+            this.BTNExcluir.TabIndex = 1;
+            this.BTNExcluir.Text = "Excluir";
+            this.BTNExcluir.UseVisualStyleBackColor = true;
+            this.BTNExcluir.Click += new System.EventHandler( this.Excluir_Click );
+            // 
+            // LBLData
+            // 
+            this.LBLData.AutoSize = true;
+            this.LBLData.Location = new System.Drawing.Point(545, 93);
+            this.LBLData.Name = "LBLData";
+            this.LBLData.Size = new System.Drawing.Size(36, 13);
+            this.LBLData.TabIndex = 3;
+            this.LBLData.Text = "Data: ";
+            // 
+            // LBLCusto
+            // 
+            this.LBLCusto.AutoSize = true;
+            this.LBLCusto.Location = new System.Drawing.Point(545, 120);
+            this.LBLCusto.Name = "LBLCusto";
+            this.LBLCusto.Size = new System.Drawing.Size(37, 13);
+            this.LBLCusto.TabIndex = 7;
+            this.LBLCusto.Text = "Custo:";
+            // 
+            // LBLDistancia
+            // 
+            this.LBLDistancia.AutoSize = true;
+            this.LBLDistancia.Location = new System.Drawing.Point(545, 147);
+            this.LBLDistancia.Name = "LBLDistancia";
+            this.LBLDistancia.Size = new System.Drawing.Size(54, 13);
+            this.LBLDistancia.TabIndex = 8;
+            this.LBLDistancia.Text = "Distancia:";
+            // 
+            // CHKCapturaSim
+            // 
+            this.CHKCapturaSim.AutoSize = true;
+            this.CHKCapturaSim.Checked = true;
+            this.CHKCapturaSim.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CHKCapturaSim.Location = new System.Drawing.Point(600, 176);
+            this.CHKCapturaSim.Name = "CHKCapturaSim";
+            this.CHKCapturaSim.Size = new System.Drawing.Size(43, 17);
+            this.CHKCapturaSim.TabIndex = 9;
+            this.CHKCapturaSim.Text = "Sim";
+            this.CHKCapturaSim.UseVisualStyleBackColor = true;
+            // 
+            // LBLNivelDor
+            // 
+            this.LBLNivelDor.AutoSize = true;
+            this.LBLNivelDor.Location = new System.Drawing.Point(545, 202);
+            this.LBLNivelDor.Name = "LBLNivelDor";
+            this.LBLNivelDor.Size = new System.Drawing.Size(52, 13);
+            this.LBLNivelDor.TabIndex = 10;
+            this.LBLNivelDor.Text = "Nivel dor:";
+            // 
+            // BTNSalvar
+            // 
+            this.BTNSalvar.Location = new System.Drawing.Point(600, 251);
+            this.BTNSalvar.Name = "BTNSalvar";
+            this.BTNSalvar.Size = new System.Drawing.Size(75, 23);
+            this.BTNSalvar.TabIndex = 11;
+            this.BTNSalvar.Text = "Salvar";
+            this.BTNSalvar.UseVisualStyleBackColor = true;
+            this.BTNSalvar.Click += new System.EventHandler(this.Salvar_Click);
+            // 
+            // BTNCancelar
+            // 
+            this.BTNCancelar.Location = new System.Drawing.Point(701, 251);
+            this.BTNCancelar.Name = "BTNCancelar";
+            this.BTNCancelar.Size = new System.Drawing.Size(75, 23);
+            this.BTNCancelar.TabIndex = 12;
+            this.BTNCancelar.Text = "Cancelar";
+            this.BTNCancelar.UseVisualStyleBackColor = true;
+            this.BTNCancelar.Click += new System.EventHandler( this.Cancelar_Click );
+            // 
+            // DBListView
+            // 
+            this.DBListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.CLNMID,
+            this.CLNMData,
+            this.CLNMCaptura,
+            this.CLNMNivelDor});
+            this.DBListView.FullRowSelect = true;
+            this.DBListView.HideSelection = false;
+            this.DBListView.Location = new System.Drawing.Point(12, 12);
+            this.DBListView.Name = "DBListView";
+            this.DBListView.Size = new System.Drawing.Size(450, 280);
+            this.DBListView.TabIndex = 13;
+            this.DBListView.UseCompatibleStateImageBehavior = false;
+            this.DBListView.View = System.Windows.Forms.View.Details;
+            this.DBListView.SelectedIndexChanged += new System.EventHandler(this.SelectListViewItem );
+            // 
+            // CLNMID
+            // 
+            this.CLNMID.Text = "ID";
+            // 
+            // CLNMData
+            // 
+            this.CLNMData.Text = "Data";
+            // 
+            // CLNMCaptura
+            // 
+            this.CLNMCaptura.Text = "Captura";
+            // 
+            // CLNMNivelDor
+            // 
+            this.CLNMNivelDor.Text = "Nivel Dor";
+            // 
+            // CHKCapturaNao
+            // 
+            this.CHKCapturaNao.AutoSize = true;
+            this.CHKCapturaNao.Location = new System.Drawing.Point(701, 176);
+            this.CHKCapturaNao.Name = "CHKCapturaNao";
+            this.CHKCapturaNao.Size = new System.Drawing.Size(46, 17);
+            this.CHKCapturaNao.TabIndex = 14;
+            this.CHKCapturaNao.Text = "Nao";
+            this.CHKCapturaNao.UseVisualStyleBackColor = true;
+            // 
+            // LBLCaptura
+            // 
+            this.LBLCaptura.AutoSize = true;
+            this.LBLCaptura.Location = new System.Drawing.Point(548, 176);
+            this.LBLCaptura.Name = "LBLCaptura";
+            this.LBLCaptura.Size = new System.Drawing.Size(47, 13);
+            this.LBLCaptura.TabIndex = 15;
+            this.LBLCaptura.Text = "Captura:";
+            // 
+            // NumNivelDor
+            // 
+            this.NumNivelDor.Location = new System.Drawing.Point(600, 202);
+            this.NumNivelDor.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.NumNivelDor.Name = "NumNivelDor";
+            this.NumNivelDor.Size = new System.Drawing.Size(176, 20);
+            this.NumNivelDor.TabIndex = 16;
+            // 
+            // DTPData
+            // 
+            this.DTPData.Location = new System.Drawing.Point(600, 91);
+            this.DTPData.Name = "DTPData";
+            this.DTPData.Size = new System.Drawing.Size(176, 20);
+            this.DTPData.TabIndex = 17;
+            // 
+            // NumCusto
+            // 
+            this.NumCusto.Location = new System.Drawing.Point(600, 118);
+            this.NumCusto.Name = "NumCusto";
+            this.NumCusto.Size = new System.Drawing.Size(176, 20);
+            this.NumCusto.TabIndex = 18;
+            // 
+            // NUMDistancia
+            // 
+            this.NUMDistancia.Location = new System.Drawing.Point(600, 147);
+            this.NUMDistancia.Name = "NUMDistancia";
+            this.NUMDistancia.Size = new System.Drawing.Size(176, 20);
+            this.NUMDistancia.TabIndex = 19;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // AcmeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 304);
+            this.Controls.Add(this.NUMDistancia);
+            this.Controls.Add(this.NumCusto);
+            this.Controls.Add(this.DTPData);
+            this.Controls.Add(this.NumNivelDor);
+            this.Controls.Add(this.LBLCaptura);
+            this.Controls.Add(this.CHKCapturaNao);
+            this.Controls.Add(this.DBListView);
+            this.Controls.Add(this.BTNCancelar);
+            this.Controls.Add(this.BTNSalvar);
+            this.Controls.Add(this.LBLNivelDor);
+            this.Controls.Add(this.CHKCapturaSim);
+            this.Controls.Add(this.LBLDistancia);
+            this.Controls.Add(this.LBLCusto);
+            this.Controls.Add(this.LBLData);
+            this.Controls.Add(this.BTNExcluir);
+            this.Controls.Add(this.BTNIncluir);
+            this.Name = "AcmeForm";
+            this.Text = "ACME Flight Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.NumNivelDor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumCusto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUMDistancia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTNIncluir;
+        private System.Windows.Forms.Button BTNExcluir;
+        private System.Windows.Forms.Label LBLData;
+        private System.Windows.Forms.Label LBLCusto;
+        private System.Windows.Forms.Label LBLDistancia;
+        private System.Windows.Forms.CheckBox CHKCapturaSim;
+        private System.Windows.Forms.Label LBLNivelDor;
+        private System.Windows.Forms.Button BTNSalvar;
+        private System.Windows.Forms.Button BTNCancelar;
+        private System.Windows.Forms.ListView DBListView;
+        private System.Windows.Forms.ColumnHeader CLNMData;
+        private System.Windows.Forms.ColumnHeader CLNMCaptura;
+        private System.Windows.Forms.ColumnHeader CLNMNivelDor;
+        private CheckBox CHKCapturaNao;
+        private Label LBLCaptura;
+        private ColumnHeader CLNMID;
+        private NumericUpDown NumNivelDor;
+        private DateTimePicker DTPData;
+        private NumericUpDown NumCusto;
+        private NumericUpDown NUMDistancia;
+        private ContextMenuStrip contextMenuStrip1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+    }
+}
+
---
+++ ThomasGregTest/AcmeForm.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThomasGregTest
+{
+    public partial class AcmeForm : Form
+    {
+        private int IdSelected { get; set; }
+
+        public AcmeForm()
+        {
+            this.InitializeComponent();
+
+            this.InitDBConnection();
+
+            this.LoadItemsToListView();
+        }
+
+        /// <summary>
+        /// Load DB data into the list view.
+        /// </summary>
+        /// <returns>True if success, false if not.</returns>
+        private bool LoadItemsToListView()
+        {
+            try
+            {
+                this.DBListView.Items.Clear();
+
+                SQLiteConnection conn = new SQLiteConnection( "Data Source=acme.sqlite" );
+                conn.Open();
+
+                SQLiteCommand cmd = conn.CreateCommand();
+
+                cmd.CommandText = "SELECT ID_VOO, DATA_VOO, CHAR(CAPTURA), NIVEL_DOR FROM TB_VOO";
+
+                using( SQLiteDataReader reader = cmd.ExecuteReader() )
+                {
+                    while( reader.Read() )
+                    {
+                        this.AddToListView( reader[0].ToString(), DateTime.Parse( reader[1].ToString() ).ToString(), reader[2].ToString(), reader[3].ToString() );
+                    }
+                }
+
+                conn.Close();
+                return true;
+            }
+            catch( Exception e )
+            {
+                Console.WriteLine( e.Message );
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Initialize a connection with the DB.
+        /// </summary>
+        /// <returns>The DB connection.</returns>
+        private SQLiteConnection InitDBConnection()
+        {
+            if( File.Exists( "acme.sqlite" ) )
+            {
+                return new SQLiteConnection( "Data Source=acme.sqlite" );
+            }
+            else
+            {
+                if( this.CretaAcmeDB() )
+                    return new SQLiteConnection( "Data Source=acme.sqlite" );
+                else
+                    return null;
+            }
+        }
+
+        /// <summary>
+        /// Creates a sqlite file with the TB_VOO table.
+        /// </summary>
+        /// <returns>True if success, false if not.</returns>
+        private bool CretaAcmeDB()
+        {
+            try
+            {
+                SQLiteConnection.CreateFile( "acme.sqlite" );
+
+                SQLiteConnection myConnection = new SQLiteConnection( "Data Source=acme.sqlite" );
+
+                myConnection.Open();
+
+                string createDb = "CREATE TABLE TB_VOO ( ID_VOO INT, DATA_VOO TEXT, CUSTO DECIMAL(10, 5), DISTANCIA INT, CAPTURA INT, NIVEL_DOR INT )";
+
+                SQLiteCommand command = new SQLiteCommand( createDb, myConnection );
+
+                command.ExecuteNonQuery();
+
+                myConnection.Close();
+
+                return true;
+            }
+            catch( Exception e )
+            {
+                Console.WriteLine( e.Message );
+
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Add one item into the list view display.
+        /// </summary>
+        /// <param name="id">TB_VOO id.</param>
+        /// <param name="data">TB_VOO data.</param>
+        /// <param name="captura">TB_VOO captura.</param>
+        /// <param name="niveldor">TB_VOO niveldor.</param>
+        private void AddToListView( string id, string data, string captura, string niveldor )
+        {
+            string[] row = { id, data, captura, niveldor };
+
+            ListViewItem item = new ListViewItem( row );
+
+            this.DBListView.Items.Add( item );
+        }         
+
+        private void Incluir_Click( object sender, EventArgs e )
+        {         
+            this.DTPData.Text = "";
+            this.NumNivelDor.Value = 0;
+            this.NumCusto.Value = 0;
+            this.NUMDistancia.Value = 0;
+            this.CHKCapturaSim.Checked = false;
+            this.CHKCapturaNao.Checked = false;
+        }
+
+        private void Salvar_Click( object sender, EventArgs e )
+        {
+            char captura = 'N';
+
+            if( this.CHKCapturaSim.Checked )
+                captura = 'S';
+            else if( this.CHKCapturaNao.Checked )
+                captura = 'N';
+
+            decimal custo = this.NumCusto.Value;
+            int distancia = (int)this.NUMDistancia.Value;
+            int nivelDor = (int)this.NumNivelDor.Value;
+            string data = this.DTPData.Text;
+
+            SQLiteConnection conn = new SQLiteConnection( "Data Source=acme.sqlite" );
+            conn.Open();
+
+            SQLiteCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = "INSERT INTO TB_VOO ( ID_VOO, DATA_VOO, CAPTURA, NIVEL_DOR, CUSTO, DISTANCIA ) VALUES ( ?, ?, ? , ?, ?, ?)";
+            cmd.Parameters.Add( new SQLiteParameter { Value = new Random().Next( 1, int.MaxValue ) } );
+            cmd.Parameters.Add(new SQLiteParameter { Value = data } );
+            cmd.Parameters.Add(new SQLiteParameter { Value = (int)captura } );
+            cmd.Parameters.Add(new SQLiteParameter { Value = nivelDor } );
+            cmd.Parameters.Add(new SQLiteParameter { Value = custo } );
+            cmd.Parameters.Add( new SQLiteParameter { Value = distancia } );
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            this.BTNSalvar.Enabled = false;
+            this.BTNSalvar.Enabled = false;
+            this.LoadItemsToListView();
+        }
+
+        private void SelectListViewItem( object sender, EventArgs e )
+        {
+           this.IdSelected = int.Parse( this.DBListView.SelectedItems[0].SubItems[0].Text);
+
+            SQLiteConnection conn = new SQLiteConnection( "Data Source=acme.sqlite" );
+            conn.Open();
+
+            SQLiteCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = "SELECT ID_VOO, DATA_VOO, CHAR(CAPTURA), NIVEL_DOR, CUSTO, DISTANCIA FROM TB_VOO WHERE ID_VOO = ?";
+            cmd.Parameters.Add( new SQLiteParameter { Value = this.IdSelected } );
+
+            using( SQLiteDataReader reader = cmd.ExecuteReader() )
+            {
+                while( reader.Read() )
+                {                                                     
+                    this.DTPData.Text = reader[1].ToString();
+
+                    if( reader[2].ToString()[0] == 'S' )
+                        this.CHKCapturaSim.Checked = true;
+                    else if( reader[2].ToString()[0] == 'N' )
+                        this.CHKCapturaNao.Checked = true;
+
+                    this.NumNivelDor.Value = Convert.ToDecimal( reader[3]);
+                    this.NumCusto.Value = Convert.ToDecimal( reader[4]);
+                    this.NUMDistancia.Value = Convert.ToDecimal( reader[5]);
+                }
+            }
+
+            conn.Close();
+
+            this.BTNSalvar.Enabled = true;
+            this.BTNSalvar.Enabled = true;
+        }
+
+        private void Cancelar_Click( object sender, EventArgs e )
+        {
+            this.BTNSalvar.Enabled = false;
+            this.BTNSalvar.Enabled = false;
+            this.LoadItemsToListView();
+        }
+
+        private void Excluir_Click( object sender, EventArgs e )
+        {
+            SQLiteConnection conn = new SQLiteConnection( "Data Source=acme.sqlite" );
+            conn.Open();
+
+            SQLiteCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = "DELETE FROM TB_VOO WHERE ID_VOO = ?";
+            cmd.Parameters.Add( new SQLiteParameter { Value = this.IdSelected } );
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            this.LoadItemsToListView();
+        }
+    }
+}
